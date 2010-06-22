55285202f2282cd55740617fb3f67c47ed077fb5
junkin <junkin@gmail.com>
2010-06-22 19:34:51 -0400

    respaced

--- atmos_rest.c
+++ atmos_rest.c
@@ -27,10 +27,10 @@ void create_ns(credentials *c, char * uri, char *content_type ,acl *acl,user_met
     http_method method = POST;
     char *headers[20];
     if(acl) {
-      //add acl headers
+	//add acl headers
     }
     if(meta) {
-      //add meta headers on create
+	//add meta headers on create
     }
     http_request_ns(c, method,uri,content_type, headers,0, NULL, ws);
     
@@ -41,10 +41,10 @@ void  update_ns (credentials *c, char * uri, char *content_type, acl *acl, postd
     char *headers[20];  
     http_method method = PUT;    
     if(acl) {
-      //add acl headers
+	//add acl headers
     }
     if(meta) {
-      //add meta headers on create
+	//add meta headers on create
     }
     
     http_request_ns(c, method, uri, content_type,headers, 0, (void*)data, ws);
@@ -70,89 +70,89 @@ int delete_ns(credentials *c, char *uri, ws_result *ws)
 }
 
 /*
-HTTP/1.1 200 OK
+  HTTP/1.1 200 OK
 
-Date: Mon, 21 Jun 2010 14:39:44 GMT
+  Date: Mon, 21 Jun 2010 14:39:44 GMT
 
-Server: Apache
+  Server: Apache
 
-x-emc-policy: default
+  x-emc-policy: default
 
-Content-Length: 0
+  Content-Length: 0
 
-x-emc-groupacl: other=NONE
+  x-emc-groupacl: other=NONE
 
-x-emc-useracl: EMC007A49DEEA84C837E=FULL_CONTROL
+  x-emc-useracl: EMC007A49DEEA84C837E=FULL_CONTROL
 
-x-emc-meta: atime=2010-06-21T14:37:37Z, mtime=2010-06-21T14:37:37Z, ctime=2010-06-21T14:37:37Z, itime=2010-06-21T14:37:37Z, type=regular, uid=EMC007A49DEEA84C837E, gid=apache, objectid=4980cdb2a310105404bc7d8ca4d51e04c1f7931a51ae, objname=capi_5th, size=0, nlink=1, policyname=default
+  x-emc-meta: atime=2010-06-21T14:37:37Z, mtime=2010-06-21T14:37:37Z, ctime=2010-06-21T14:37:37Z, itime=2010-06-21T14:37:37Z, type=regular, uid=EMC007A49DEEA84C837E, gid=apache, objectid=4980cdb2a310105404bc7d8ca4d51e04c1f7931a51ae, objname=capi_5th, size=0, nlink=1, policyname=default
 
-X-Cnection: close
+  X-Cnection: close
 
-Content-Type: application/octet-stream
+  Content-Type: application/octet-stream
 
- */
+*/
 
 
 
 void parse_headers(ws_result* ws, system_meta* sm, user_meta* ptr_um) {
-  int i = 0;
-  for(; i < ws->header_count; i++) {
+    int i = 0;
+    for(; i < ws->header_count; i++) {
 
-    if(0==strncmp(ws->headers[i], EMC_META_HDR_STR, strlen(EMC_META_HDR_STR))) {
-	sscanf(ws->headers[i], "x-emc-meta: atime=%s mtime=%s ctime=%s itime=%s type=%s uid=%s gid=%s objectid=%s objname=%s size=%d, nlink=%d, policyname=%s\n", sm->atime,sm->mtime, sm->ctime, sm->itime, sm->type, sm->uid, sm->gid, sm->objectid, sm->objectname, &sm->size, &sm->nlink, sm->policyname);
+	if(0==strncmp(ws->headers[i], EMC_META_HDR_STR, strlen(EMC_META_HDR_STR))) {
+	    sscanf(ws->headers[i], "x-emc-meta: atime=%s mtime=%s ctime=%s itime=%s type=%s uid=%s gid=%s objectid=%s objname=%s size=%d, nlink=%d, policyname=%s\n", sm->atime,sm->mtime, sm->ctime, sm->itime, sm->type, sm->uid, sm->gid, sm->objectid, sm->objectname, &sm->size, &sm->nlink, sm->policyname);
       
-      sm->atime[strlen(sm->atime)-1] = '\0';
-      sm->mtime[strlen(sm->mtime)-1] = '\0';
-      sm->ctime[strlen(sm->ctime)-1] = '\0';
-      sm->itime[strlen(sm->itime)-1] = '\0';      
-      sm->type[strlen(sm->type)-1] = '\0';
-      sm->uid[strlen(sm->uid)-1] = '\0';
-      sm->gid[strlen(sm->gid)-1] = '\0';
-      sm->objectid[strlen(sm->objectid)-1] = '\0';
-      sm->objectname[strlen(sm->objectname)-1] = '\0';
+	    sm->atime[strlen(sm->atime)-1] = '\0';
+	    sm->mtime[strlen(sm->mtime)-1] = '\0';
+	    sm->ctime[strlen(sm->ctime)-1] = '\0';
+	    sm->itime[strlen(sm->itime)-1] = '\0';      
+	    sm->type[strlen(sm->type)-1] = '\0';
+	    sm->uid[strlen(sm->uid)-1] = '\0';
+	    sm->gid[strlen(sm->gid)-1] = '\0';
+	    sm->objectid[strlen(sm->objectid)-1] = '\0';
+	    sm->objectname[strlen(sm->objectname)-1] = '\0';
 
-    } else if(0==strncmp(ws->headers[i], EMC_USER_HDR_STR, strlen(EMC_USER_HDR_STR))) {
-      printf("USERACL: %s\n", ws->headers[i]);
+	} else if(0==strncmp(ws->headers[i], EMC_USER_HDR_STR, strlen(EMC_USER_HDR_STR))) {
+	    printf("USERACL: %s\n", ws->headers[i]);
       
-    } else if(0 == strncmp(ws->headers[i], EMC_LISTABLE_META_HDR_STR, strlen(EMC_LISTABLE_META_HDR_STR))) {
-      //listable x-emc-listable-meta: meta_test=meta_pass
-      char *pairs[1024];
-      int count = 0;
-      split(ws->headers[i]+strlen(EMC_LISTABLE_META_HDR_STR), ',', pairs, &count);
-      int index = 0;
-      for(;index < count; index++) {
-	ptr_um = malloc(sizeof(user_meta));
-	bzero(ptr_um, sizeof(user_meta));
+	} else if(0 == strncmp(ws->headers[i], EMC_LISTABLE_META_HDR_STR, strlen(EMC_LISTABLE_META_HDR_STR))) {
+	    //listable x-emc-listable-meta: meta_test=meta_pass
+	    char *pairs[1024];
+	    int count = 0;
+	    split(ws->headers[i]+strlen(EMC_LISTABLE_META_HDR_STR), ',', pairs, &count);
+	    int index = 0;
+	    for(;index < count; index++) {
+		ptr_um = malloc(sizeof(user_meta));
+		bzero(ptr_um, sizeof(user_meta));
 
-	ptr_um->listable = true;	
-	char *key_value[2];
-	int kv_count = 0;
-	split(pairs[index], '=', key_value, &kv_count);
-	if(kv_count ==2) {
-	  strcpy(ptr_um->key,key_value[0]);
-	  strcpy(ptr_um->value,key_value[1]);
-	} else {
-	  printf("meta data parse error!\n");
-	}
-	int k;
-	for(k=0; k<=kv_count; k++) {
-	  free(key_value[k]);
-	}
-	ptr_um = ptr_um->next;
-      }
+		ptr_um->listable = true;	
+		char *key_value[2];
+		int kv_count = 0;
+		split(pairs[index], '=', key_value, &kv_count);
+		if(kv_count ==2) {
+		    strcpy(ptr_um->key,key_value[0]);
+		    strcpy(ptr_um->value,key_value[1]);
+		} else {
+		    printf("meta data parse error!\n");
+		}
+		int k;
+		for(k=0; k<=kv_count; k++) {
+		    free(key_value[k]);
+		}
+		ptr_um = ptr_um->next;
+	    }
 
-      int free_pairs = 0;
-      for(free_pairs=0; free_pairs<=count; free_pairs++) {
-	free(pairs[count]);
-      }
+	    int free_pairs = 0;
+	    for(free_pairs=0; free_pairs<=count; free_pairs++) {
+		free(pairs[count]);
+	    }
 
       
 	
-    } else if(0 == strncmp(ws->headers[i], EMC_META_HDR_STR, strlen(EMC_META_HDR_STR))) {
-      ptr_um->listable=false;
+	} else if(0 == strncmp(ws->headers[i], EMC_META_HDR_STR, strlen(EMC_META_HDR_STR))) {
+	    ptr_um->listable=false;
       
+	}
     }
-  }
 }
 
 
@@ -162,9 +162,9 @@ void parse_headers(ws_result* ws, system_meta* sm, user_meta* ptr_um) {
 // type=regular, uid=EMC007A49DEEA84C837E, gid=apache, objectid=4980cdb2a510105804bfc45d19680d04c0f8d1a1b587, objname=capi_5th, size=0, nlink=1, policyname=default
 
 void get_system_meta(ws_result *ws, system_meta *meta) {
-  if(ws && meta) {
-    ;
-  }
+    if(ws && meta) {
+	;
+    }
     //char *meta_string = ws->headers;
     //printf("%s\n", meta_string);
     
